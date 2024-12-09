90daaeff26c748d2c2e624294c3fb0dd8cbd9fb6
gicziAttila <83330738+gicziAttila@users.noreply.github.com>
2024-12-09 13:44:24 +0100

    RecipeCard javítva

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script setup>
 import { ref } from 'vue'
-import Card from "@/components/Card.vue";
+import Card from "@/components/RecipeCard.vue";
 </script>
 
 <template>
   <header>
-    <h1>App</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand">Receptkönyv</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav justify-content-center me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="">Receptek</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Új recept</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Kedvencek</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
   <main>
     <Card />
