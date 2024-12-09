9f89c11098c37e91ddb807259ea2e825de7c575a
gicziAttila <83330738+gicziAttila@users.noreply.github.com>
2024-12-09 13:39:24 +0100

    Kártya alapok

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
+import { ref } from 'vue'
+import Card from "@/components/Card.vue";
 </script>
 
 <template>
+  <header>
+    <h1>App</h1>
+  </header>
+  <main>
+    <Card />
+  </main>
+  <footer>
+  </footer>
 </template>
 
 <style scoped>
---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card" style="width: 301px;">
+    <img src="../assets/palacsinta.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h3 class="card-title mb-3">Palacsinta</h3>
+      <h6 class="card-subtitle mb-4 text-body-secondary">Elkészítési idő: 30 perc</h6>
+      <p class="card-text text-white w-25 mb-4 text-center bg-success border rounded">könnyű</p>
+      <a href="#" class="btn btn-primary">Részletek</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  img{
+    height: 200px;
+    width: 300px;
+    object-fit: cover;
+  }
+</style>
