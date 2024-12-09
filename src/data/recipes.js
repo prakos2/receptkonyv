cdd6547eb09f674166e7bb85fb9e37c519e4ef9a
preilakos <preilakos@gmail.com>
2024-12-09 13:26:06 +0100

    Dummy adatok

---
+++ src/data/recipes.js
@@ -0,0 +1,22 @@
+export default recipes = [
+    {
+        title: "Palacsinta",
+        time: 30,
+        difficulty: "easy"
+    },
+    {
+        title: "Paprikás krumpli",
+        time: 45,
+        difficulty: "easy"
+    },
+    {
+        title: "Túrógombóc",
+        time: 60,
+        difficulty: "medium"
+    },
+    {
+        title: "Gulyásleves",
+        time: 120,
+        difficulty: "hard"
+    },
+]
