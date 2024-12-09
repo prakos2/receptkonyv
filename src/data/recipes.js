8e885ec62c5cc59caa056773fd05ee5ea6bedd99
preilakos <preilakos@gmail.com>
2024-12-09 13:29:01 +0100

    dummy update

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,20 +1,24 @@
-export default recipes = [
+export const recipes = [
     {
+        id: 0,
         title: "Palacsinta",
         time: 30,
         difficulty: "easy"
     },
     {
+        id: 1,
         title: "Paprikás krumpli",
         time: 45,
         difficulty: "easy"
     },
     {
+        id: 2,
         title: "Túrógombóc",
         time: 60,
         difficulty: "medium"
     },
     {
+        id: 3,
         title: "Gulyásleves",
         time: 120,
         difficulty: "hard"
