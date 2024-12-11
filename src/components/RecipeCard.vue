60929b1c37e00c1ba08362136337ed3ef41fc9f0
gicziAttila <83330738+gicziAttila@users.noreply.github.com>
2024-12-11 18:15:28 +0100

    Hover effektet teszt

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -12,7 +12,7 @@ const props = defineProps({
   <div class="card" style="width: 600px;">
     <img :src="props.recipe.getImgURL()" class="card-img-top img-fluid" alt="...">
     <div class="card-body">
-      <h3 class="card-title mb-3">{{ props.recipe.getTitle() }}</h3>
+      <h3 class="card-title mb-3 textHoverEffect" :data-hover="props.recipe.getTitle()">{{ props.recipe.getTitle() }}</h3>
       <h6 class="card-subtitle mb-4 text-body-secondary">
         Elkészítési idő: {{ props.recipe.getTime() }} perc
       </h6>
@@ -20,7 +20,7 @@ const props = defineProps({
       <div class="d-flex flex-column align-items-start">
         <!-- Badge -->
         <p
-            class="btn text-white text-center border rounded px-2 py-1 mb-2"
+            class="text-white text-center border rounded px-2 py-1 mb-2 diff"
             :class="badges[props.recipe.getDifficulty()].class"
         >
           {{ badges[props.recipe.getDifficulty()].name }}
@@ -39,5 +39,20 @@ img {
   height: 200px;
   width: 599px;
   object-fit: cover;
+}
+/* hover effect */
+.card {
+  transition: all 0.3s;
+}
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  background: linear-gradient(to top , rgba(0,128,0,0.5), rgba(255,255,255,1));
+
+
+
+}
+.card:hover .diff {
+  filter: brightness(150%);
 }
 </style>
